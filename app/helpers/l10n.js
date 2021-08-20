db1589519f0c99dbf3c17c68dd8156b2a3e2aae6
KaMeHb-UA <marlock@bassteam.ml>
2021-08-20 21:58:27 +0300

    i10n

---
+++ app/helpers/l10n.js
@@ -0,0 +1,20 @@
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+import { resolve } from 'path';
+import { env } from 'process';
+import { fileURLToPath } from 'url';
+
+const l10nDir = resolve(fileURLToPath(import.meta.url), '..', '..', 'l10n');
+const defaultPhrases = load(readFileSync(resolve(l10nDir, 'en.yml'), 'utf8'));
+const localizedPhrases = {};
+try{ Object.assign(localizedPhrases, load(readFileSync(resolve(l10nDir, env.LANG + '.yml'), 'utf8'))) } catch(e){}
+
+function processTpl(text, args){
+    for(const name in args) text.replaceAll(`%${name}%`, args[name]);
+}
+
+export default (phrase, args) => {
+    if(phrase in localizedPhrases) return processTpl(localizedPhrases[phrase], args);
+    if(phrase in defaultPhrases) return processTpl(defaultPhrases[phrase], args);
+    return phrase;
+}
