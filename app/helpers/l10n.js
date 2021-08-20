36832f64a0a4d24c3bad00f7e6897ab94619d249
KaMeHb-UA <marlock@bassteam.ml>
2021-08-20 22:10:10 +0300

    fix HTML

--- app/helpers/l10n.js
+++ app/helpers/l10n.js
@@ -9,8 +9,15 @@ const defaultPhrases = load(readFileSync(resolve(l10nDir, 'en.yml'), 'utf8'));
 const localizedPhrases = {};
 try{ Object.assign(localizedPhrases, load(readFileSync(resolve(l10nDir, env.LANG + '.yml'), 'utf8'))) } catch(e){}
 
+function fixHTML(rawText){
+    return rawText
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+}
+
 function processTpl(text, args){
-    for(const name in args) text.replaceAll(`%${name}%`, args[name]);
+    for(const name in args) text.replaceAll(`%${name}%`, fixHTML(args[name]));
 }
 
 export default (phrase, args) => {
