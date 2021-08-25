d70b41eacd425d547b8ed542df55b2bf1cffb007
KaMeHb-UA <marlock@bassteam.ml>
2021-08-25 09:21:59 +0300

    direction icon

--- app/components/publish.js
+++ app/components/publish.js
@@ -1,4 +1,12 @@
 import send from '../helpers/send-message.js';
 import translate from '../helpers/l10n.js';
 
-export default ({ type, data }) => send(translate(type, data))
+export default async ({ type, data }) => {
+    if(data.amount < 0){
+        data.amount = -data.amount;
+        data.direction_icon = '⇱';
+    } else {
+        data.direction_icon = '⇲';
+    }
+    await send(translate(type, data))
+}
